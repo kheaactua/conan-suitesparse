68481d6b909acc4e731800f6bbb3e4a9b6373ba7
Matthew Russell <mrussell@neptec.com>
2018-08-28 10:56:03 -0400

    Removed all linux stuff from recipe

--- conanfile.py
+++ conanfile.py
@@ -3,82 +3,25 @@
 
 import os
 from conans import ConanFile, tools, CMake, MSBuild
-from conans.errors import ConanException
 
-# The Windows portion was inspired by https://github.com/ComFreek/suitesparse-conan-pkg
+# The Windows portion was inspired by
+# https://github.com/ComFreek/suitesparse-conan-pkg
 
 
 class SuiteSparseConan(ConanFile):
     name        = 'suitesparse'
-    version     = '5.2.0' # TODO windows version might be 5.1.2
+    version     = '5.1.2'
     license     = 'AMD License: BSD 3-clause'
-    description = "Suite of sparse matrix software.  Current windows install is actually 5.1.2, not 5.2.0!"
+    description = 'Suite of sparse matrix software.'
     url         = 'https://github.com/kheaactua/conan-suitesparse'
-    settings    = 'os', 'compiler', 'build_type', 'arch'
     generators  = 'cmake'
     requires    = (
         'helpers/[>=0.3]@ntc/stable',
     )
-    options         = {
-        'blas':        ['openblas', 'system'], # System basically doesn't do anything which should search system paths
-    }
-    default_options = (
-        'blas=system'
-    )
 
-    # Hash for Linux download
-    md5_hash  = '8e625539dbeed061cc62fbdfed9be7cf'
-
-
-    def build_requirements(self):
-        pack_names = None
-        if tools.os_info.linux_distro == "ubuntu":
-            pack_names = ['liblapack-dev', 'build-essential']
-            if 'system' == self.options.blas:
-                # Note: openblas doesn't exist on tegra.
-                pack_names.append('libopenblas-dev')
-
-            if self.settings.arch == "x86":
-                full_pack_names = []
-                for pack_name in pack_names:
-                    full_pack_names += [pack_name + ":i386"]
-                pack_names = full_pack_names
-
-        if pack_names:
-            installer = tools.SystemPackageTool()
-            try:
-                installer.update() # Update the package database
-                installer.install(" ".join(pack_names)) # Install the package
-            except ConanException:
-                self.output.warn('Could not run system updates to fetch build requirements.')
-
-    def requirements(self):
-        if 'openblas' == self.options.blas:
-            self.requires('openblas/[>=0.2.20]@ntc/stable')
+    settings = {'os': ['Windows'], 'compiler': None, 'build_type': None, 'arch': None}
 
     def source(self):
-        if 'Linux' == self.settings.os: self._source_linux()
-        else: self._source_win()
-
-    def _source_linux(self):
-        archive = 'SuiteSparse-%s.tar.gz'%self.version
-
-        from source_cache import copyFromCache
-        if not copyFromCache(archive):
-            tools.download('http://faculty.cse.tamu.edu/davis/SuiteSparse/%s'%archive, archive)
-        tools.check_md5(archive, self.md5_hash)
-
-        tools.unzip(archive)
-
-        if 'openblas' in self.deps_cpp_info.deps:
-            # LDFLAGS isn't used prevasively enough, so we need to hack the Makefile.
-            tools.replace_in_file(
-                file_path='SuiteSparse/SuiteSparse_config/SuiteSparse_config.mk',
-                search='BLAS = -lopenblas',
-                replace='BLAS = -L%s -lopenblas'%(self.deps_cpp_info['openblas'].rootpath + '/lib'),
-            )
-
-    def _source_win(self):
         self.run("git clone --depth 1 https://github.com/ComFreek/suitesparse-metis-for-windows.git suitesparse")
         self.run("cd suitesparse")
 
@@ -91,20 +34,6 @@ include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
 
     def build(self):
-        if 'Linux' == self.settings.os: self._build_linux()
-        else: self._build_win()
-
-    def _build_linux(self):
-        env_vars = {}
-
-        if 'openblas' in self.deps_cpp_info.deps:
-            env_vars['LDFLAGS']         = '-L%s/lib'%self.deps_cpp_info['openblas'].rootpath,
-            env_vars['LD_LIBRARY_PATH'] = '%s/lib'%self.deps_cpp_info['openblas'].rootpath,
-
-        with tools.environment_append(env_vars):
-            self.run('cd SuiteSparse && make -j %d'%tools.cpu_count())
-
-    def _build_win(self):
         cmake = CMake(self)
         cmake.configure(source_folder="suitesparse")
 
@@ -127,16 +56,6 @@ conan_basic_setup()''')
         # we'd need to hack the solution file to set the install path.
 
     def package(self):
-        if 'Linux' == self.settings.os: self._package_linux()
-        else: self._package_win()
-
-    def _package_linux(self):
-        self.copy(pattern='*',  dst='lib',      src=os.path.join('SuiteSparse', 'lib'),      excludes='.gitignore')
-        self.copy(pattern='*',  dst='share',    src=os.path.join('SuiteSparse', 'share'),    excludes='.gitignore')
-        self.copy(pattern='*',  dst='bin',      src=os.path.join('SuiteSparse', 'bin'),      excludes='.gitignore')
-        self.copy(pattern='*',  dst='include/suitesparse', src=os.path.join('SuiteSparse', 'include'),  excludes='.gitignore')
-
-    def _package_win(self):
         self.copy("**/*.lib", dst="lib", keep_path=False)
         self.copy("**/*.dll", dst="bin", keep_path=False)
 
@@ -146,17 +65,13 @@ conan_basic_setup()''')
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.bindirs = ['bin']
+        # Some directories must be globally visible because
+        # some files (e.g. umfpack/umfpack.h) try to include from
+        # these without specifying a relative path.
+        self.cpp_info.includedirs = ["include", "include/amd", "include/suitesparse"]
 
-        if 'Windows' == self.settings.os:
-            # Some directories must be globally visible because
-            # some files (e.g. umfpack/umfpack.h) try to include from
-            # these without specifying a relative path.
-            self.cpp_info.includedirs = ["include", "include/amd", "include/suitesparse"]
-            # self.cpp_info.libs = ["suitesparseconfig.lib", "libumfpack"]
-
-            # Add the DLLs to the RUNPATH
-            self.env_info.path.append(os.path.join(self.package_folder, 'lapack_windows', 'x32' if 'x86' == self.settings.arch else 'win64'))
-        elif 'Linux' == self.settings.os:
-            self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, 'lib'))
+        # Add the DLLs to the RUNPATH
+        self.env_info.path.append(os.path.join(self.package_folder, 'lapack_windows', 'x32' if 'x86' == self.settings.arch else 'win64'))
 
 # vim: ts=4 sw=4 expandtab ffs=unix ft=python foldmethod=marker :
